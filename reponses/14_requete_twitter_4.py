4a230cbe245beb8f7ef5cf1a103a67c68195c2ff
Olivier Grisel <olivier.grisel@ensta.org>
2009-05-30 01:18:25 +0200

    finished twitter exo

--- reponses/14_requete_twitter_4.py
+++ reponses/14_requete_twitter_4.py
@@ -6,6 +6,8 @@ import xml.etree.cElementTree as etree
 # definition d'une constante qui contient le tag recherche
 ATOM_NAMESPACE = "{http://www.w3.org/2005/Atom}"
 ENTRY_TAG = ATOM_NAMESPACE + "entry"
+AUTHOR_NAME_TAG = ATOM_NAMESPACE + "author" + "/" + ATOM_NAMESPACE + "name"
+CONTENT_TAG = ATOM_NAMESPACE + "content"
 
 # demande de saisie du mot clef a l'utilisateur
 mot_clef = raw_input('Tapper le mot clef: ')
@@ -18,5 +20,7 @@ f = urllib2.urlopen(requete)
 
 # parsing du XML contenu dans la reponse de la requete
 arbre = etree.parse(f)
-print arbre.findall(ENTRY_TAG)
+for entry in arbre.findall(ENTRY_TAG):
+    print entry.find(AUTHOR_NAME_TAG).text, ":",
+    print entry.find(CONTENT_TAG).text
 
